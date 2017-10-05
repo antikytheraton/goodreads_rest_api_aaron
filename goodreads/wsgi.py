4f34776952ea578c217350cb0f796f2073f1238c
Aaron Arredondo Sanchez <binauralvoice@gmail.com>
2017-10-04 21:18:38 -0500

    lol

--- goodreads/wsgi.py
+++ goodreads/wsgi.py
@@ -12,14 +12,14 @@ import os
 from django.core.wsgi import get_wsgi_application
 
 #Config for AWS
-from dotenv import load_dotenv
-try:
-    # este setea en automatico las variables de entorno
-    dotenv_path = os.path.join(os.path.dirname(os.path.dirname(os.path.abspath(__file__))), '.env')
-    load_dotenv(dotenv_path)
-except:
-    pass
-#--------------------------------------------
+# from dotenv import load_dotenv
+# try:
+#     # este setea en automatico las variables de entorno
+#     dotenv_path = os.path.join(os.path.dirname(os.path.dirname(os.path.abspath(__file__))), '.env')
+#     load_dotenv(dotenv_path)
+# except:
+#     pass
+# #--------------------------------------------
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "goodreads.settings.production")
 
 application = get_wsgi_application()
