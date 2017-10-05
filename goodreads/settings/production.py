f01c8aecf153c39d712c5398f7bad50377be7ef9
Aaron Arredondo Sanchez <binauralvoice@gmail.com>
2017-10-04 21:22:35 -0500

    lol

--- goodreads/settings/production.py
+++ goodreads/settings/production.py
@@ -7,23 +7,23 @@ DEBUG = False
 ALLOWED_HOSTS = ['*']
 
 # Configuration for AWS
-DATABASES = {'default': {'ENGINE': 'django.db.backends.postgresql_psycopg2',
-                         'NAME': os.getenv("DB_NAME"),
-                         'USER': os.getenv("DB_USER"),
-                         'PASSWORD': os.getenv("DB_PASSWORD"),
-                         'HOST': 'localhost',
-                         'PORT': '5432'
-                         }
-             }
+# DATABASES = {'default': {'ENGINE': 'django.db.backends.postgresql_psycopg2',
+#                          'NAME': os.getenv("DB_NAME"),
+#                          'USER': os.getenv("DB_USER"),
+#                          'PASSWORD': os.getenv("DB_PASSWORD"),
+#                          'HOST': 'localhost',
+#                          'PORT': '5432'
+#                          }
+#              }
 
 
-''' Configuration for heroku
-# DATABASES = dict()
+''' Configuration for heroku'''
+DATABASES = dict()
 
-# DATABASES['default'] = dj_database_url.config() # Heroku configuration
+DATABASES['default'] = dj_database_url.config() # Heroku configuration
 
-# SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO','https')
-'''
+SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO','https')
+
 
 
 STATIC_ROOT = os.path.join(os.getcwd(),'static')
